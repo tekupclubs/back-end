f590cd1832fb84e398465b1d942ac37ad4756490
ahmedbouderbala <ahmadbouderbala@gmail.com>
2021-11-08 13:31:22 +0100

    Mini Dashboard back-end For the server

---
+++ src/main/java/com/example/demo/monitoring/CustomActuatorEndpoint.java
@@ -0,0 +1,22 @@
+package com.example.demo.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id="custom")
+@Component
+public class CustomActuatorEndpoint {
+
+    @ReadOperation
+    public Map<String, String> customEndpoint(String username){
+        Map<String,String> map = new HashMap<>();
+        map.put("Key", "Value");
+        map.put("Username", username);
+        return map;
+
+    }
+}
